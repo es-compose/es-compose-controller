85da3645233731b79d00b76fbd114ac3d73caac8
Alamin <al.ahmed@outlook.com>
2017-04-19 09:00:18 -0400

    Add more unit tests on Controller

--- test/controllerTest.js
+++ test/controllerTest.js
@@ -5,18 +5,57 @@ const Controller = require('../');
 class TestController extends Controller
 {
     doIndex() {
-        
+        return "Hello";
     }
+
+    doGet(context, id) {
+        return Promise.resolve('Promised');
+    }
+}
+
+let next = function(...args) {
+    console.log('next called', args);
+}
+
+let context = {
+    request : {
+        method: 'get',
+        url: '/',
+        path: '',
+        params: {}
+    },
+    response:  {
+        send(content) {
+            this.content = content;
+        }
+    },
+
+    next: next
+    
 }
 
 
 describe("Controller", () => {
     let controller = new Controller();
+    let tc = new TestController();
 
     it("should return express handler", () => {
         let handler = controller.handler();
         expect(handler).be.a('function');
     })
+
+    it("should handle simple return data", async function() {
+        let result = await tc.invoke(context, next);
+        expect(context.response.content).to.be.eq("Hello");
+    })
+
+
+    it("should handle promise return type", async () => {
+        context.request.params['param'] = 'value';
+        let result = await tc.invoke(context, next);
+        expect(context.response.content).to.be.eq("Promised");
+    })
+
 
 
 });
